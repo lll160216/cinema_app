7369bbd710b496e79bcdc356272f9f2ff755df3c
Long <lll160216@gmail.com>
2022-05-12 17:11:25 +0700

    hil

--- app/src/main/java/thanhlong/example/helloworld/appcinea/CinemaServiceInterface.java
+++
@@ -1,6 +0,0 @@
-package thanhlong.example.helloworld.appcinea;
-
-public interface CinemaServiceInterface {
-    public String callHello();
-    public String callLogin(String username, String password);
-}
